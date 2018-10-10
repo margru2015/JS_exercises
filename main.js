9bebab3acc57d73e34d3a0558f75f7f6a9006dce
Martyna Grudzka <martyna.grudzka@capgemini.com>
2018-10-10 14:22:05 +0200

    Second Task ==> MODULE PATTERN

--- main.js
+++ main.js
@@ -1,23 +1,44 @@
-function personFactory () {
+var person = (function() {
     var details = {
         firstName: 'Jan',
-        lastName: 'Kowalski',
-        accounts_list: [
-            {balance: 2131,
-            currency: 'euro'}
-        ]
-    }
+        lastName: 'Kowalski'
+    
+    };
+    var calculateBalance = function() {
+        var sum=0;
+        for(var i=0; i < this.accounts_list.length; i++){
+            sum += this.accounts_list[i].balance;            
+        } 
+        return sum};
     return {
         firstName: details.firstName,
         lastName: details.lastName,
+        accounts_list: [
+            {balance: 2131,
+            currency: 'euro'},
+            {balance: 7654,
+             currency: 'euro'}],
         sayHello: function (){
-            return 'First Name: ' + this.firstName + ', Last Name: ' + this.lastName + ', number of accounts: ' + details.accounts_list.length;
-        }
-    }
-}
+            return 'First Name: ' + this.firstName + 
+            ', Last Name: ' + this.lastName + 
+            ', number of accounts: ' + this.accounts_list.length +
+            ', total balance: ' + calculateBalance.call(this);
+            
+        },
+        addAccount: function (newBalance, newCurrency){
+            this.accounts_list.push ({
+               balance: newBalance,
+               currency: newCurrency 
+            })
 
-person1 = personFactory();
-console.log(person1.sayHello());
+        },
+        
+    };
+})();
 
+console.log(person.sayHello());
 
+person.addAccount(1000, 'euro');
+
+console.log(person.sayHello());
 
